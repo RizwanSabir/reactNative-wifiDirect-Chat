0a9e209b287aa77da2e2191f7cf5aae9daabc528
Rizwan321 <125357675+RizwanSabir@users.noreply.github.com>
2024-05-20 05:05:32 -0700

    Update Firebase.jsx

--- Firebase/Firebase.jsx
+++ Firebase/Firebase.jsx
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDIhrb6v3rXMvkuT_4Bi96y-Wwk_6MMWmQ",
-    authDomain: "introverse-3d38b.firebaseapp.com",
-    projectId: "introverse-3d38b",
-    storageBucket: "introverse-3d38b.appspot.com",
-    messagingSenderId: "419100033003",
-    appId: "1:419100033003:web:888eb1f5ebf762245ea3ae"
+    // apiKey: "AIzaSyDIhrb6v3rXMvkuT_4Bi96y-Wwk_6MMWmQ",
+    // authDomain: "introverse-3d38b.firebaseapp.com",
+    // projectId: "introverse-3d38b",
+    // storageBucket: "introverse-3d38b.appspot.com",
+    // messagingSenderId: "419100033003",
+    // appId: "1:419100033003:web:888eb1f5ebf762245ea3ae"
 };
 
 const app = initializeApp(firebaseConfig);
